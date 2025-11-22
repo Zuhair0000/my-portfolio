05aa3d4dcfa462fbb539466a23c4732e292c7141
Zuhair Ahmed <153817334+Zuhair0000@users.noreply.github.com>
2025-11-22 12:13:28 +0800

    fix About section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,5 +1,5 @@
 import { useEffect, type ReactNode } from "react";
-import { Code, Palette, Smartphone } from "lucide-react";
+import { Code, Cpu, Palette, ServerCog, Smartphone } from "lucide-react";
 import Aos from "aos";
 import "aos/dist/aos.css";
 import SectionWrap from "./SectionWrap.js";
@@ -54,7 +54,7 @@ function TiltCard({ children }: { children: ReactNode }) {
         rotateY,
         transformStyle: "preserve-3d",
       }}
-      className="relative bg-gradient-to-br from-blue-500 to-purple-600 h-80 rounded-2xl"
+      className="relative bg-gradient-to-br from-blue-500 to-purple-600 h-90 rounded-2xl"
     >
       {children}
     </motion.div>
@@ -70,19 +70,28 @@ export default function About() {
     <SectionWrap
       id="about"
       title="What I Do"
-      text="I build responsive and engaging websites, crafting intuitive user interfaces that elevate the user experience. My work combines full-stack web development with thoughtful UI/UX design, ensuring functionality, performance, and visual appeal. With expertise in React.js + Typescript, Node.js, MySQL, and modern tools like Tailwind CSS, Docker, and cloud services, I deliver seamless, scalable digital solutions tailored to real-world needs."
+      text="I build responsive and engaging websites, crafting intuitive user interfaces that elevate the user experience. My work combines full-stack web development, ensuring functionality, performance, and visual appeal. With expertise in React.js + Typescript, Node.js, MySQL, and modern tools like Tailwind CSS, Docker, cloud services, and AI / ML experimentation I deliver seamless, scalable digital solutions tailored to real-world needs."
       className="bg-gray-50"
     >
-      <div className="grid md:grid-cols-2 gap-8 lg:gap-12">
+      <div className="grid md:grid-cols-3 gap-8 lg:gap-12">
         <TiltCard>
           <div
+            style={{
+              transformStyle: "preserve-3d",
+              transform: "translateZ(75px)",
+            }}
             className="absolute inset-4 group bg-white rounded-2xl p-8 shadow-lg hover:shadow-2xl transition-all duration-500 hover:-translate-y-2"
             data-aos="fade-up"
             data-aos-delay="100"
           >
-            <div className="flex flex-col items-center text-center">
+            <div
+              style={{
+                transform: "translateZ(75px)",
+              }}
+              className="flex flex-col items-center text-center"
+            >
               <div className="w-20 h-20 bg-gradient-to-br from-blue-500 to-purple-600 rounded-2xl flex items-center justify-center mb-6 group-hover:scale-110 transition-transform duration-300">
-                <Code className="w-10 h-10 text-white transform transition-transform duration-500 group-hover:rotate-180" />
+                <Code className="w-10 h-10 text-white transform transition-transform duration-500  group-hover:rotate-360" />
               </div>
               <h3 className="text-2xl font-bold text-gray-900 mb-4">
                 Web Development
@@ -97,21 +106,51 @@ export default function About() {
 
         <TiltCard>
           <div
+            style={{
+              transformStyle: "preserve-3d",
+              transform: "translateZ(75px)",
+            }}
             className="absolute inset-4 group bg-white rounded-2xl p-8 shadow-lg hover:shadow-2xl transition-all duration-500 hover:-translate-y-2"
             data-aos="fade-up"
             data-aos-delay="200"
           >
             <div className="flex flex-col items-center text-center">
-              <div className="w-20 h-20 bg-gradient-to-br from-pink-500 to-orange-500 rounded-2xl flex items-center justify-center mb-6 group-hover:scale-110 transition-transform duration-300">
-                <Palette className="w-10 h-10 text-white transform transition-transform duration-500 group-hover:rotate-180" />
+              <div className="w-20 h-20 bg-gradient-to-br from-green-400 to-teal-600 rounded-2xl flex items-center justify-center mb-6 group-hover:scale-110 transition-transform duration-300">
+                <ServerCog className="w-10 h-10 text-white transform transition-transform duration-500 group-hover:rotate-180" />
               </div>
               <h3 className="text-2xl font-bold text-gray-900 mb-4">
-                UX/UI Design
+                DevOps & Deployment
               </h3>
               <p className="text-gray-600 leading-relaxed">
-                I design intuitive and visually appealing interfaces, focusing
-                on user needs and enhancing usability across web and mobile
-                platforms.
+                I streamline deployment pipelines and maintain scalable
+                infrastructure using Docker, Docker Compose, GitHub Actions,
+                AWS, and cloud hosting solutions.
+              </p>
+            </div>
+          </div>
+        </TiltCard>
+
+        <TiltCard>
+          <div
+            style={{
+              transformStyle: "preserve-3d",
+              transform: "translateZ(75px)",
+            }}
+            className="absolute inset-4 group bg-white rounded-2xl p-8 shadow-lg hover:shadow-2xl transition-all duration-500 hover:-translate-y-2"
+            data-aos="fade-up"
+            data-aos-delay="200"
+          >
+            <div className="flex flex-col items-center text-center">
+              <div className="w-20 h-20 bg-gradient-to-br from-purple-500 to-pink-500  rounded-2xl flex items-center justify-center mb-6 group-hover:scale-110 transition-transform duration-300">
+                <Cpu className="w-10 h-10 text-white transform transition-transform duration-500 group-hover:rotate-180" />
+              </div>
+              <h3 className="text-2xl font-bold text-gray-900 mb-4">
+                AI / ML (Learning)
+              </h3>
+              <p className="text-gray-600 leading-relaxed">
+                I experiment with Machine Learning, Deep Learning, and Model
+                Deployment to expand my skills beyond traditional web
+                development.
               </p>
             </div>
           </div>
